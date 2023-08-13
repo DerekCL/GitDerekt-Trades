457e130a4d96b98cc4322a59ccfbdb3e78e18d98
derek.lewandowski <derek.lewandowski@coda.global>
2023-08-13 12:21:24 -0500

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as Discord from "discord.js";
+import { Client, Message } from "discord.js";
 
 // dotenv is a package that allows us to store sensitive information in a .env file
 require("dotenv").config();
 
-const client: Discord.Client = new Discord.Client({
+const client: Client = new Client({
   intents: [
     "Guilds",
     "GuildMessages",
@@ -17,7 +17,7 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-client.on("message", (msg: Discord.Message) => {
+client.on("message", (msg: Message) => {
   if (msg.content.startsWith("!echo")) {
     // Remove the command and send the remaining text back
     const textToSend: string = msg.content.replace("!echo ", "");
